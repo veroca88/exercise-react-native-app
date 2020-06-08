92495247f25daed7b76ef36d8ef3df55a3a1be98
veroca88 <veronicadonoso1506@gmail.com>
2020-06-08 14:29:20 -0400

    Install expo-constants and create a new component Screen

---
+++ app/components/Screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Constants from "expo-constants";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+
+function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
+
+export default Screen;
