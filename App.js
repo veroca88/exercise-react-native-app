d681b3fd962621e0386b31b27c51b7d560b7bf32
veroca88 <veronicadonoso1506@gmail.com>
2020-06-07 21:45:45 -0400

    Create Card component

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Button from "./app/screens/AppButton";
+import Card from "./app/screens/Card";
+
 import { View } from "react-native";
 
 export default function App() {
-  return <WelcomeScreen />;
+  // return <WelcomeScreen />;
   // return <ViewImageScreen />;
 
   // return (
@@ -19,4 +21,20 @@ export default function App() {
   //     <Button title={"Login"} />
   //   </View>
   // );
+
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 50,
+      }}
+    >
+      <Card
+        title={"Red Jacket for sale"}
+        subtitle={"$100"}
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
---
+++ app/screens/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AppText from "./AppText";
+import colors from "../config/colors";
+
+import { View, Image, StyleSheet } from "react-native";
+
+function Card({ title, subtitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image source={image} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: colors.white,
+    borderRadius: 15,
+    marginBottom: 20,
+    overflow: "hidden", //it helps to our image get the radius of 15
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  subtitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+  text: {
+    marginBottom: 7,
+  },
+});
+
+export default Card;
