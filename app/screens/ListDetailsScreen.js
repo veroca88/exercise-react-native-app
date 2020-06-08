7d202a423ffdae5022d8d42bcf1efb75d0aa52d3
veroca88 <veronicadonoso1506@gmail.com>
2020-06-08 11:39:54 -0400

    Create Details screen

---
+++ app/screens/ListDetailsScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import AppText from "./components/AppText";
+import colors from "../config/colors";
+import ListItem from "./components/ListItems";
+
+import { View, Image, StyleSheet } from "react-native";
+
+function ListDetails(props) {
+  return (
+    <View>
+      <Image source={require("../assets/jacket.jpg")} style={styles.image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red Jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+        {/* If we want to aplly some property to our component BUT we want to have that 
+        property only in that screen is better aproach if we go to the screen page and wrap it inside the container <View>
+        and aplly in that container the style desired.   */}
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/vero.png")}
+            title="Vero Donoso"
+            subtitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 24,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 30,
+  },
+});
+
+export default ListDetails;
