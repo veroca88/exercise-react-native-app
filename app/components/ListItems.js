267aa4a894058a910002a115276fedc4a50be186
veroca88 <veronicadonoso1506@gmail.com>
2020-06-10 14:48:24 -0400

    Add conditional for image and subtitle

--- app/components/ListItems.js
+++ app/components/ListItems.js
@@ -5,16 +5,24 @@ import AppText from "./AppText";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 
-function ListItems({ title, subtitle, image, onPress, renderRightActions }) {
+function ListItems({
+  title,
+  subtitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       {/* Without onPress method nothing happens */}
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image source={image} style={styles.image} />
-          <View>
+          {ImageComponent}
+          {image && <Image source={image} style={styles.image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subtitle}>{subtitle}</AppText>
+            {subtitle && <AppText style={styles.subtitle}>{subtitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -26,11 +34,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 15,
   },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subtitle: {
     color: colors.medium,
