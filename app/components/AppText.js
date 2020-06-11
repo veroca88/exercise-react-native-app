b1364ff2af2b06d744fc5c14bd9ff341621460e3
veroca88 <veronicadonoso1506@gmail.com>
2020-06-11 16:06:37 -0400

    Extract common styles in one file

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    padding: 3,
-  },
-});
+// We create a file in config folder because the code below we have repetition in other file AppTextInput.js as well.
+// So now we import the file defaultStyles and use it in the <Text> component. It makes cleaner and more mantainable
+// const styles = StyleSheet.create({
+//   text: {
+//     fontSize: 18,
+//     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+//     padding: 3,
+//   },
+// });
 
 export default AppText;
