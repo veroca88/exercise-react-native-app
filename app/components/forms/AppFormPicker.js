b80082b7cf8c357da7afe9eeb5e2040489bc526c
veroca88 <veronicadonoso1506@gmail.com>
2020-06-14 17:12:20 -0400

    Create reusable component of AppPicker and ErrorMessage

---
+++ app/components/forms/AppFormPicker.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppPicker from "../AppPicker";
+import ErrorMessage from "./ErrorMessage";
+
+function AppFormPicker({ items, name, placeholder }) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  return (
+    <>
+      <AppPicker
+        items={items}
+        onSelectItem={(item) => setFieldValue(item)}
+        selectedItem={values[name]}
+        placeholder={placeholder}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormPicker;
