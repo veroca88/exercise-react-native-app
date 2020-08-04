305428fb9d5063c19f22b4278c499af0c5da82a8
veroca88 <veronicadonoso1506@gmail.com>
2020-08-04 11:11:57 -0400

    Add scrollview horizontal, icon to add automatically appears at right side

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imagesUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+  // we added View wrapp scrollview due to scrollview takes all the heigth and width of
+  // our screen, adding View it only takes the height and width of the content
   return (
-    <View style={styles.container}>
-      {imagesUris.map((uri) => (
-        <ImageInput
-          key={uri}
-          imageUri={uri}
-          onChangeImage={() => onRemoveImage(uri)}
-        />
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imagesUris.map((uri) => (
+            <ImageInput
+              key={uri}
+              imageUri={uri}
+              onChangeImage={() => onRemoveImage(uri)}
+            />
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
